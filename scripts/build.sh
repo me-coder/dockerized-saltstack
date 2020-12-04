d41667f1fa86836853a8153782a9ff879d3a6d36
Yashodhan Pise <technoy@gmail.com>
2020-12-04 17:53:29 +0530

    First bugfix for this repo.
    Signed-off-by: Yashodhan Pise <technoy@gmail.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,6 +4,6 @@ BASEDIR=$(dirname "${BASH_SOURCE}")
 pushd ${BASEDIR}
 
 docker build --rm --tag greenhorn:salt-minion-centos8 --file ../minion/Dockerfile .
-docker build --rm --tag greenhorn:salt-minion-centos8 --file ../master/Dockerfile .
+docker build --rm --tag greenhorn:salt-master-centos8 --file ../master/Dockerfile .
 
 popd
