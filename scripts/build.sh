b53525ba8cce9ba6495a6d56fdc273fb60ce6d20
Yashodhan Pise <technoy@gmail.com>
2021-05-30 00:11:15 +0530

    Updates to dockerfiles for centos7.8
    Signed-off-by: Yashodhan Pise <technoy@gmail.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,7 +3,9 @@
 BASEDIR=$(dirname "${BASH_SOURCE}")
 pushd ${BASEDIR}
 
-docker build --rm --tag greenhorn:salt-minion-centos8 --file ../minion/Dockerfile .
-docker build --rm --tag greenhorn:salt-master-centos8 --file ../master/Dockerfile .
+# docker build --rm --tag greenhorn:salt-minion-centos8 --file ../minion/Dockerfile .
+# docker build --rm --tag greenhorn:salt-master-centos8 --file ../master/Dockerfile .
+docker build --rm --tag greenhorn:salt-minion-centos --file ./minion/Dockerfile.multistage .
+docker build --rm --tag greenhorn:salt-master-centos --file ./master/Dockerfile.multistage .
 
 popd
